8b7ba0ea60174f426e57e70ff90d2acf50130a3a
sakshi singh <thakursakshisingh8318@gmail.com>
2025-10-04 11:52:52 +0530

    Why choose Us section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import FeatureCourses from "@/components/FeatureCourses";
 import HeroSection from "@/components/HeroSection";
+import WhyChosseUs from "@/components/WhyChosseUs";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection/>
       <FeatureCourses/>
+      <WhyChosseUs/>
     </main>
   );
 }
